f635ec58f6caf111c5face5015d34cd1c8890fa7
liuge6668 <liuge6668@gmail.com>
2025-11-22 21:23:25 +0800

    init antd

---
+++ antd_pro/config/defaultSettings.ts
@@ -0,0 +1,23 @@
+import type { ProLayoutProps } from '@ant-design/pro-components';
+
+/**
+ * @name
+ */
+const Settings: ProLayoutProps & {
+  pwa?: boolean;
+  logo?: string;
+} = {
+  "navTheme": "light",
+  "colorPrimary": "#FAAD14",
+  "layout": "side",
+  "contentWidth": "Fluid",
+  "fixedHeader": true,
+  "fixSiderbar": true,
+  "pwa": true,
+  "logo": "https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg",
+  "token": {},
+  "splitMenus": false,
+  "siderMenuType": "sub"
+};
+
+export default Settings;
---
+++ antd_pro/src/defaultSettings.js
@@ -0,0 +1,11 @@
+export default {
+  navTheme: "light",
+  colorPrimary: "#FA541C",
+  layout: "mix",
+  contentWidth: "Fluid",
+  fixedHeader: false,
+  fixSiderbar: true,
+  pwa: true,
+  logo: "https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg",
+  token: {},
+};
